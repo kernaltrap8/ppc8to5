ab5230314e083c325b94aed1a4c4c71d21d636db
kernaltrap8 <kernaltrap@gmail.com>
2024-06-10 13:32:51 -0500

    early code
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

--- src/ppc8to5.c
+++ src/ppc8to5.c
@@ -7,7 +7,8 @@ int main() {
   //  would be vxor, which takes the values
   //  of two registers then zeros them, and puts
   //  them in a resulting register.
-  asm("li vs0, 0x20");
+  asm("li vs0, 0x20" : "=r"(regval));
+  printf("0x%x", regval);
   asm("vxor, vs0, vs0, vs0");
   asm("li r1, vs0" : "=r"(regval));
   printf("0x%x", regval);
