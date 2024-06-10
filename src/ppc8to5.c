86704f879832e885763593c84748e2c3a49473e4
kernaltrap8 <kernaltrap@gmail.com>
2024-06-10 13:29:14 -0500

    early code
    Signed-off-by: kernaltrap8 <kernaltrap@gmail.com>

---
+++ src/ppc8to5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int regval;
+  // asm("xxlxor vs0, vs0, vs0");
+  //  the alternative to xxlxor in P5
+  //  would be vxor, which takes the values
+  //  of two registers then zeros them, and puts
+  //  them in a resulting register.
+  asm("li vs0, 0x20");
+  asm("vxor, vs0, vs0, vs0");
+  asm("li r1, vs0" : "=r"(regval));
+  printf("0x%x", regval);
+  return 0;
+}
